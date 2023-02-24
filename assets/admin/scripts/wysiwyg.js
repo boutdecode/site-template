7157963f704a79002d453d82b3d91b72eb7f8cdc
Kevin <kevinbalicot@gmail.com>
2023-02-24 13:17:37 +0100

    feat: Add code editor for WYSIWYG

--- assets/admin/scripts/wysiwyg.js
+++ assets/admin/scripts/wysiwyg.js
@@ -5,12 +5,16 @@ import "tinymce/icons/default";
 import "tinymce/plugins/image";
 import "tinymce/plugins/link";
 import "tinymce/plugins/lists";
+import "tinymce/plugins/fullscreen";
+import "tinymce/plugins/preview";
+import "tinymce/plugins/importcss";
+import "../vendor/tinymce/codeeditor";
 
 export default () => {
     document.querySelectorAll('.wysiwyg').forEach(target => {
         tinymce.init({
             target,
-            plugins: ['image', 'link', 'lists'],
+            plugins: ['image', 'link', 'lists', 'fullscreen', 'code', 'preview', 'importcss'],
             toolbar: [
                 { name: 'history', items: ['undo', 'redo'] },
                 { name: 'styles', items: ['styles'] },
@@ -19,7 +23,9 @@ export default () => {
                 { name: 'alignment', items: ['alignleft', 'aligncenter', 'alignright', 'alignjustify'] },
                 { name: 'indentation', items: ['outdent', 'indent'] },
                 { name: 'insert', items: ['link', 'image'] },
+                { name: 'tools', items: ['fullscreen', 'code', 'preview'] }
             ],
+            content_css: '/assets/front/front.css',
             setup: function (editor) {
                 editor.on('change', () => {
                     if (target instanceof HTMLInputElement) {
---
+++ assets/admin/vendor/tinymce/codeeditor/index.js
@@ -0,0 +1,104 @@
+import { EditorState } from "@codemirror/state";
+import { EditorView, basicSetup } from "codemirror";
+import { indentWithTab } from "@codemirror/commands";
+import { keymap } from "@codemirror/view";
+import { html } from "@codemirror/lang-html";
+
+(function () {
+    'use strict';
+
+    const global = tinymce.util.Tools.resolve('tinymce.PluginManager');
+    let codeEditor = null;
+
+    const setContent = (editor) => {
+        editor.focus();
+        editor.undoManager.transact(() => {
+            editor.setContent(codeEditor.state.doc.toString());
+        });
+        editor.selection.setCursorLocation();
+        editor.nodeChanged();
+    };
+    const getContent = editor => {
+        return editor.getContent({source_view: true});
+    };
+
+    const open = editor => {
+        const editorContent = getContent(editor);
+        const panel = editor.windowManager.open({
+            title: 'Source Code',
+            size: 'large',
+            body: {
+                type: 'panel',
+                items: [{
+                    type: 'htmlpanel',
+                    html: `
+                        <style>
+                            #code-editor {
+                                position: relative;
+                                width: 100%;
+                                height: 500px;
+                            }
+                        </style>
+                        <div id="code-editor"></div>
+                    `
+                }]
+            },
+            buttons: [
+                {
+                    type: 'cancel',
+                    name: 'cancel',
+                    text: 'Cancel'
+                },
+                {
+                    type: 'submit',
+                    name: 'save',
+                    text: 'Save',
+                    primary: true
+                }
+            ],
+            onSubmit: api => {
+                setContent(editor, api.getData().code);
+                api.close();
+            }
+        });
+
+        codeEditor = new EditorView({
+            state: EditorState.create({
+                extensions: [basicSetup, keymap.of([indentWithTab]), html()],
+                doc: editorContent,
+            }),
+            parent: document.querySelector('#code-editor'),
+        });
+    };
+
+    const register$1 = editor => {
+        editor.addCommand('mceCodeEditor', () => {
+            open(editor);
+        });
+    };
+
+    const register = editor => {
+        const onAction = () => editor.execCommand('mceCodeEditor');
+        editor.ui.registry.addButton('code', {
+            icon: 'sourcecode',
+            tooltip: 'Source code',
+            onAction
+        });
+        editor.ui.registry.addMenuItem('code', {
+            icon: 'sourcecode',
+            text: 'Source code',
+            onAction
+        });
+    };
+
+    var Plugin = () => {
+        global.add('code', editor => {
+            register$1(editor);
+            register(editor);
+            return {};
+        });
+    };
+
+    Plugin();
+
+})();
